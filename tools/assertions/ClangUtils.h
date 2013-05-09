d167bbdf5c227d6a700520f017ecb221c6842297
Dan Sanduleac <sanduleac.dan@gmail.com>
2013-05-09 16:21:33 +0100

    Removed ClangUtils.

--- tools/assertions/ClangUtils.h
+++
@@ -1,9 +0,0 @@
-#ifndef ClangUtils
-#define ClangUtils
-
-#include "clang/Rewrite/Rewriter.h"
-#include "clang/Basic/SourceManager.h"
-#include "clang/Lex/Lexer.h"
-#include "clang/AST/Stmt.h"
-
-#endif
