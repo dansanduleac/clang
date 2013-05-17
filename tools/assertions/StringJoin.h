d55e316e2deac3515c6181b3d6bd8e309a56a09d
Dan Sanduleac <sanduleac.dan@gmail.com>
2013-05-17 13:25:12 +0100

    StringJoin improvements; Sep included in Concatenation

--- tools/assertions/StringJoin.h
+++ tools/assertions/StringJoin.h
@@ -1,6 +1,7 @@
 #ifndef ANNOTATEVARIABLES_STRINGJOIN_H
 #define ANNOTATEVARIABLES_STRINGJOIN_H
 
+#include "llvm/ADT/ArrayRef.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Compiler.h" // LLVM_LIKELY
 
@@ -16,7 +17,16 @@ namespace dont_use {
   template<typename To> char convertible_to_helper(const volatile To);
   template<typename To> double convertible_to_helper(...);
 
+  // I can't call this function from outside without having to
+  // import it with "using".
   template<typename T> nothing operator<<(raw_ostream &, T);
+
+  template <typename T>
+  struct is_writeable {
+    static raw_ostream &OS;
+    static T &Elem;
+    static const bool value = sizeof(nothing) != sizeof(OS << Elem);
+  };
 }
 
 template <typename From, typename To>
@@ -26,15 +36,10 @@ struct is_convertible_to {
 };
 
 namespace {
-  using dont_use::operator<<;
+  //using dont_use::operator<<;
   using dont_use::nothing;
+  using dont_use::is_writeable;
 
-  template <typename T>
-  struct is_writeable {
-    static raw_ostream &OS;
-    static T &Elem;
-    static const bool value = sizeof(nothing) != sizeof(OS << Elem);
-};
 }
 
 // Optionally:
@@ -47,11 +52,14 @@ namespace {
 
 class Concatenation : public raw_string_ostream {
   std::string S;
+  StringRef Sep;
   bool start = true;
 public:
-  Concatenation() : raw_string_ostream(S) {}
+  //Concatenation() : raw_string_ostream(S) {}
+  Concatenation(StringRef separator = ",")
+    : raw_string_ostream(S), Sep(separator) {}
 
-  void writeSep(StringRef separator) {
+  inline void writeSep(StringRef separator) {
     if (LLVM_UNLIKELY(start)) {
       start = false;
     } else {
@@ -60,9 +68,14 @@ public:
   }
 
   template <class T>
-  inline Concatenation &appendJoin(T Strings, StringRef separator) {
+  inline Concatenation &append(T Strings, StringRef separator) {
     return join(Strings, separator, *this);
   }
+
+  template <class T>
+  inline Concatenation &append(T Strings) {
+    return join(Strings, Sep, *this);
+  }  
 };
 
 template <typename T>
@@ -76,6 +89,7 @@ join(ArrayRef<T> Strings, StringRef separator, Concatenation &Out) {
 }
 
 template <class T>
+// typename llvm::enable_if<is_base_of<ArrayRef<U>, T>, Concatenation&>::type
 inline Concatenation &join(T Strings, StringRef separator,
                            Concatenation &Out) {
   join(makeArrayRef(Strings), separator, Out);
